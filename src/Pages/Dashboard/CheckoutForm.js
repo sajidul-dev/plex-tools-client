76d0e094bc38ddc3906541ca5da3ab4ddef21c01
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-26 20:30:37 +0600

    Payment info save to db

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,13 +1,38 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import Loading from '../Shared/Loading';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ tool }) => {
+    const { price, name, email, _id } = tool
 
     const stripe = useStripe();
     const elements = useElements();
 
     const [cardError, setCardError] = useState('')
+    const [clientSecret, setClientSecret] = useState("")
     const [success, setSuccess] = useState('')
+    const [transactionId, setTransactionId] = useState('')
+    const [processing, setProcessing] = useState(false)
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                "authorization": `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret)
+                }
+            })
+
+    }, [price])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -28,8 +53,60 @@ const CheckoutForm = () => {
             setSuccess('')
         } else {
             setCardError('')
+            setProcessing(true)
         }
+
+
+        // confirming card payment
+
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: name,
+                        email: email
+                    },
+                },
+            },
+        );
+
+        if (intentError) {
+            setCardError(intentError?.message)
+            setSuccess('')
+            setProcessing(false)
+        }
+        else {
+            setCardError('')
+            setTransactionId(paymentIntent.id)
+            setSuccess("Congrats! Your payment is completed")
+
+
+            // update payment to db
+            const payment = {
+                toolId: _id,
+                transactionId: paymentIntent.id
+            }
+            fetch(`http://localhost:5000/order/${_id}`, {
+                method: "PATCH",
+                headers: {
+                    "content-type": "application/json",
+                    "authorization": `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(payment)
+
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setProcessing(false)
+                    console.log(data);
+                    toast.success("Payment successful")
+                })
+        }
+
     }
+
 
     return (
         <>
@@ -50,14 +127,14 @@ const CheckoutForm = () => {
                         },
                     }}
                 />
-                <button className='btn btn-xs btn-success mt-4' type="submit" disabled={!stripe}>
+                <button className='btn btn-xs btn-success mt-4' type="submit" disabled={!stripe || !clientSecret}>
                     Pay
                 </button>
             </form>
             {cardError && <p className='text-red-500'>{cardError}</p>}
-            {success && <div className='text-green-500'>
+            {success && <div className='text-green-600'>
                 <p>{success}</p>
-                {/* <p>Your transaction Id: <span className='text-orange-500 font-bold'>{transactionId}</span></p> */}
+                <p>Your transaction Id: <span className='text-orange-500 font-bold'>{transactionId}</span></p>
             </div>}
         </>
     );
